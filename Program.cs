a4a03664e3eb94ecaba043e7086d24f791cedcfd
Daniel <daniyus05@gmail.com>
2023-04-18 12:38:41 +0200

    nombre invertido

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Por favor, escriba su nombre: ");
+var nombre = Console.ReadLine();
+int longitudNombre = nombre.Length;
+
+var nombreInvertido = "";
+for (int i = longitudNombre - 1; i >= 0; i--)
+{
+    nombreInvertido = nombreInvertido + nombre[i];
+}
+Console.WriteLine("Adiós " + nombreInvertido);
